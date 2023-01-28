59d45d6eb82ac332edd828546d70f90e9ca964fa
Ilix-studio <ilishjyoti17@gmail.com>
2023-01-29 01:49:11 +0530

    Create Post UI done

--- frontend/src/page/CreatePost.jsx
+++ frontend/src/page/CreatePost.jsx
@@ -15,7 +15,8 @@ const CreatePost = () => {
   const [loading, setLoading] = useState(false);
   const handleSubmit = () => {};
   const handleChange = () => {};
-  const handleSurpriseMe = () => {}
+  const handleSurpriseMe = () => {};
+  const generateImage = () => {};
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -36,16 +37,61 @@ const CreatePost = () => {
             value={form.name}
             onChange={handleChange}
           />
-          <FormField 
-          labelName="Prompt"
-          type="text"
-          name="prompt"
-          placeholder="A futuristic cyborg dance club, neon lights"
-          value={form.prompt}
-          onChange={handleChange}
-          isSurpriseMe
-          handleSurpriseMe={handleSurpriseMe}
+          <FormField
+            labelName="Prompt"
+            type="text"
+            name="prompt"
+            placeholder="A futuristic cyborg dance club, neon lights"
+            value={form.prompt}
+            onChange={handleChange}
+            isSurpriseMe
+            handleSurpriseMe={handleSurpriseMe}
           />
+
+          <div className="relative bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-64 p-3 h-64 flex justify-center items-center">
+            {form.photo ? (
+              <img
+                src={form.photo}
+                alt={form.prompt}
+                className="w-full h-full object-contain"
+              />
+            ) : (
+              <img
+                src={preview}
+                alt="preview"
+                className="w-9/12 h-9/12 object-contain opacity-40"
+              />
+            )}
+            {generatingImg && (
+              <div className="absolute inset-0 z-0 flex justify-center items-center bg-[rgba(0,0,0,0.5)] rounded-lg">
+                <Loader />
+              </div>
+            )}
+          </div>
+        </div>
+
+        <div>
+          <button
+            type="button"
+            onClick={generateImage}
+            className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {generatingImg ? "Generating..." : "Generate"}
+          </button>
+        </div>
+
+        <div className="mt-10">
+          <p className="mt-2 text-[#666e75] text-[14px]">
+            ** Once you have created the image you want, you can share it with
+            others in the community **
+          </p>
+          <button
+            type="submit"
+            className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+          >
+            {" "}
+            {loading ? "Sharing..." : "Share with the Community"}
+          </button>
         </div>
       </form>
     </section>
